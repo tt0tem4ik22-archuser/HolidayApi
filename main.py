8c5baebe3296d68025a9fca7d8f1e55b6ea9f800
slava2324_couldnt_login( <nncorp6@gmail.com>
2026-04-20 19:59:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import os
 from dotenv import load_dotenv
 import requests
-import json
+
+
+MONTHS = ["Января","Февраля","Марта","Апреля","Мая","Июня","Июля","Августа","Сентября","Октября","Ноября","Декабря",]
 
 
 def main():
 
     load_dotenv()
     api_key = os.getenv("API_KEY")
+    params = {
+    	api_key="{api_key}",
+    	country="RU",
+    	year=2025
+    }
+    link = f"https://calendarific.com/api/v2/holidays"
 
-    link = f"https://calendarific.com/api/v2/holidays?&api_key={api_key}&country=US&year=2025"
-
-    response = requests.get(link)
+    response = requests.get(link, params=params)
     response.raise_for_status()
     
     for holiday in response.json()["response"]["holidays"]:
-        print(f"""Дата: {holiday['date']['datetime']['day']}.{holiday['date']['datetime']['month']},
+        print(f"""Дата: {holiday['date']['datetime']['day']} {MONTHS[holiday['date']['datetime']['month']-1]},
 Название: {holiday['name']}
 Описание: {holiday['description']}\n""")
 
